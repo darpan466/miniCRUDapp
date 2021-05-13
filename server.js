41dd56d3b74934094427ce542840dbca8728de1a
Darpan Singh <darpan466@users.noreply.github.com>
2021-05-13 13:01:41 +0530

    Added user model, user controller and user route to the app

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
-/* Loading environment variables */
 import dotenv from "dotenv";
-dotenv.config();
-/*****/
-
 import express from "express";
+import mongoose from "mongoose";
+import userRoutes from "./routes/user.js";
 
-const app = express();
+dotenv.config();
+const App = express();
 
-
-app.get("/", (req, res) => res.send(process.env.MONGO_DB_URL));
+App.use(express.json());
+App.use(express.urlencoded({extended: true}));
+App.use("/api/user", userRoutes);
 
 const PORT = process.env.CONNECTION_URL ?? 3000;
+const CONNECTION_URL = process.env.MONGO_DB_URL;
 
-app.listen(PORT, () => console.log(`Server running on: http://localhost:${PORT}`));
+(async () => {
+  const mongooseConfigOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  }; 
+  try {
+    await mongoose.connect(CONNECTION_URL, mongooseConfigOptions);
+    App.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+      console.log(`Database is running on ${CONNECTION_URL}`);
+    });
+  } catch(error) {
+    console.log(error);
+  }
+})();
+
+
 
 
 
